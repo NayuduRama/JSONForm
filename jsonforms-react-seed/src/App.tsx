10651304be0391c2394de32e7de81da96779bed8
NayuduRama <sadamramanayudu@gmail.com>
2021-11-01 08:11:11 +1100

    Routers added

--- jsonforms-react-seed/src/App.tsx
+++ jsonforms-react-seed/src/App.tsx
@@ -1,135 +1,29 @@
-import { Fragment, useState, useEffect } from 'react';
-import { JsonForms } from '@jsonforms/react';
-import Grid from '@material-ui/core/Grid';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-import './App.css';
-import schema from './schema.json';
-import uischema from './uischema.json';
-import axios from 'axios';
+ 
+import './App.css'; 
 import {
-  materialCells,
-  materialRenderers,
-} from '@jsonforms/material-renderers';
-import RatingControl from './RatingControl';
-import ratingControlTester from './ratingControlTester';
-import { makeStyles } from '@material-ui/core/styles'; 
+  BrowserRouter as Router,
+  Route,
+  Switch,
+} from 'react-router-dom'
+import Home from './components/Home';
+import CreateForm from './components/CreateForm'; 
+import PageNotFound from './components/PageNotFound';
 
-const useStyles = makeStyles((_theme) => ({
-  container: {
-    padding: '1em',
-    width: '100%',
-  },
-  title: {
-    textAlign: 'center',
-    padding: '0.25em',
-  },
-  dataContent: {
-    display: 'flex',
-    justifyContent: 'center',
-    borderRadius: '0.25em',
-    backgroundColor: '#cecece',
-    marginBottom: '1rem',
-  },
-  resetButton: {
-    margin: 'auto',
-    display: 'block',
-  },
-  demoform: {
-    margin: 'auto',
-    padding: '1rem',
-  },
-}));
-
-const initialData = {
-  name: 'User Name',
-  email: 'Enter your email',
-  password: 'Password',
-};
-
-const renderers = [
-  ...materialRenderers,
-  //register custom renderers
-  { tester: ratingControlTester, renderer: RatingControl },
-];
-
-const App = () => {
-  const classes = useStyles();
-  const [displayDataAsString, setDisplayDataAsString] = useState('');
-  const [jsonformsData, setJsonformsData] = useState<any>(initialData);
-
-  useEffect(() => {
-    setDisplayDataAsString(JSON.stringify(jsonformsData, null, 2));
-  }, [jsonformsData]);
-
-  const clearData = () => {
-    setJsonformsData({});
-  };
-  const submitData = () => {
-    const user = {
-      name: jsonformsData.name,
-      email: jsonformsData.email,
-      password: jsonformsData.password,
-    };   
-    axios.post('/api/users', user).then((res) => console.log(res.data));
-  }; 
+const App = () => { 
 
   return (
-    <Fragment>
-      <div className='App'>
-        <header className='App-header'>
-          <h1 className='App-title'>Welcome to JSON Forms with React</h1>
-          <p className='App-intro'>More Forms. Less Code.</p>
-        </header>
-      </div>
-
-      <Grid
-        container
-        justify={'center'}
-        spacing={1}
-        className={classes.container}
-      >
-        <Grid item sm={6}>
-          <Typography variant={'h3'} className={classes.title}>
-            JSON Form
-          </Typography>
-          <div className={classes.dataContent}>
-            <pre id='boundData'>{displayDataAsString}</pre>
-          </div>
-          <Button
-            className={classes.resetButton}
-            onClick={clearData}
-            color='primary'
-            variant='contained'
-          >
-            Clear data
-          </Button>
-        </Grid>
-        <Grid item sm={6}>
-          <Typography variant={'h3'} className={classes.title}>
-            Rendered form
-          </Typography>
-          <div className={classes.demoform}>
-            <JsonForms
-              schema={schema}
-              uischema={uischema}
-              data={jsonformsData}
-              renderers={renderers}
-              cells={materialCells}
-              onChange={({ errors, data }) => setJsonformsData(data)}
-            />
-            <Button
-              className={classes.resetButton}
-              onClick={submitData}
-              color='primary'
-              variant='contained'
-            >
-              Submit
-            </Button>
-          </div>
-        </Grid>
-      </Grid> 
-      </Fragment>
+    // <CreateForm /> 
+    <Router>
+            <div className="App"> 
+              <div id="page-body">
+                <Switch>
+                <Route path="/" component={Home} exact/>
+                <Route path="/createform" component={CreateForm} exact/>  
+                <Route component={PageNotFound}/>
+                </Switch>
+              </div>
+            </div>
+          </Router> 
   );
 };
 
---
+++ jsonforms-react-seed/src/components/Home.jsx
@@ -0,0 +1,15 @@
+ 
+import '../App.css';
+
+function Home(props) {
+    return (
+        <div className='App'>
+            <header className='App-header'>
+            <h1 className='App-title'>Welcome to JSON Forms with React</h1>
+            <p className='App-intro'>More Forms. Less Code.</p>
+            </header>
+        </div>
+    );
+}
+
+export default Home;
